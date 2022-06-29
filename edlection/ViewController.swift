995b4797cde75725e6933552711775292374296d
evelyntsoi <evelyn.jy.tsoi@gmail.com>
2022-06-28 18:26:45 -0700

    Initial Commit

---
+++ edlection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  edlection
+//
+//  Created by Evelyn T. on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
